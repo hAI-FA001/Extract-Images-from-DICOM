4cf1e3fbb8b17b5c15e431ce8642ef925fc02860
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-11-18 17:18:01 +0500

    use os independent directory separator

--- final/main.py
+++ final/main.py
@@ -3,9 +3,10 @@ import os
 from utils import process_dicoms
 
 if __name__ == "__main__":
-    base_dir = "../"
+    SLASH = os.sep
+    base_dir = ".."
 
-    dicom_files = files = [f"{base_dir}{f}" for f in os.listdir(base_dir) if 'dcm' in os.path.splitext(f)[-1].lower()]
+    dicom_files = files = [f"{base_dir}{SLASH}{f}" for f in os.listdir(base_dir) if 'dcm' in os.path.splitext(f)[-1].lower()]
     if len(dicom_files) > 0:
         process_dicoms(dicom_files, save_images=True)
     else:
