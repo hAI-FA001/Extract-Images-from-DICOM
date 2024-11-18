42ec0514dea641010d8093b4432cf205a98525ce
HaiderAbbas <114867851+hAI-FA001@users.noreply.github.com>
2024-11-18 17:16:59 +0500

    some error handling + useful messages

--- final/main.py
+++ final/main.py
@@ -6,5 +6,8 @@ if __name__ == "__main__":
     base_dir = "../"
 
     dicom_files = files = [f"{base_dir}{f}" for f in os.listdir(base_dir) if 'dcm' in os.path.splitext(f)[-1].lower()]
-    process_dicoms(dicom_files, save_images=True)
+    if len(dicom_files) > 0:
+        process_dicoms(dicom_files, save_images=True)
+    else:
+        print("Found no dicom files. Make sure they end in .dcm")
 
--- final/utils.py
+++ final/utils.py
@@ -8,8 +8,13 @@ def read_items(f):
     def read_offsets_table():
         group, element = struct.unpack('<HH', f.read(4))
         tag = group << 16 | element
+        if tag != 0xFFFEE000:
+            print(f"{tag:0x}")
+            raise ValueError('Incorrect tag for Offset Table')
         
         table_len = struct.unpack('<I', f.read(4))[0]
+        if table_len % 4 != 0:
+            raise ValueError('Offset Table length must be a multiple of 4')
         
         return list(struct.unpack(f'<{table_len // 4}L', f.read(table_len)))
     
@@ -24,9 +29,14 @@ def read_items(f):
         if tag == 0xFFFEE000:
             item_len = struct.unpack('<I', f.read(4))[0]
 
+            if item_len == 0xFFFFFFFF:
+                raise ValueError('Undefined Item Length')
+            
             frag.append(f.read(item_len))
         elif tag == 0xFFFEE0DD:
             break
+        else:
+            raise ValueError(f"Incorrect tag found while reading items: {tag:0x}\nExpected Item Tag 0xFFFEE000 or Sequence Delimiter Tag 0xFFFEE0DD")
 
     return frag
 
@@ -42,7 +52,7 @@ def parse_img(fragments):
 
     return imgs
 
-def process_dicoms(files):
+def process_dicoms(files, save_images=False):
     def find_signature_bytes(f):
         # look for (7FE0, 0010) with VR code OB
         sign_bytes = [0xE0, 0x7f, 0x10, 0x0, 0x4f, 0x42]
@@ -65,11 +75,19 @@ def process_dicoms(files):
         fname = fname.split(os.sep)[-1]
 
         with open(dicom, "rb") as f:
+            f.seek(0, os.SEEK_END)
+            file_size = f.tell()
+            f.seek(0, os.SEEK_SET)
+
             while True:
                 try:
                     if not find_signature_bytes(f):
                         continue
                 except:
+                    if f.tell() != file_size:
+                        print(f'Unknown Error at offset: {f.tell()}')
+                    else:
+                        print(f"Finished reading {dicom}")
                     break
                 
 
@@ -79,7 +97,13 @@ def process_dicoms(files):
                 is_undef_len = data_elem_len == 0xFF_FF_FF_FF
 
                 if not is_undef_len:
+                    print(f"Expected undefined length but got {data_elem_len:0x}. Skipping...")
                     continue
                 
+                print("Extracting raw images...")
                 imgs = parse_img(read_items(f))
                 imgs = np.array(imgs)
+                
+                print(f'Read {len(imgs)} images')
+        print()
+
